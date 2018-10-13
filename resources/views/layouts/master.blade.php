afc87ff8cc233ce8a5bca52af58e2fbc919bc78c
Mikayil S <sert.mikayil@gmail.com>
2018-10-13 20:56:19 +0200

    Adding html attributs on master layout.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name') }}</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('app/css') }}" />
     </head>
     <body>
-        <h1>Hello World</h1>
+
+        <div class="wrapper">
+            @yield('container')
+        </div>
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
