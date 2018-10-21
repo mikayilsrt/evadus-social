fe58c5fdc7eee5ad54a8f90ec5d1ab64d9f03f56
Mikayil S <sert.mikayil@gmail.com>
2018-10-22 00:55:26 +0200

    :ok_hand: Updating the navbar on desktop and mobile.

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,26 @@
 
 require('./bootstrap');
 
+/**
+ * Require the bootstrap javascript file.
+ **/
 require('bootstrap/dist/js/bootstrap');
 
 // require('semantic-ui-sass/semantic-ui.js');
+
+/**
+ * Mobile nav click event.
+ */
+const mobileMenuButtons = document.querySelectorAll(".btn-nav-toogle");
+const mobileNav = document.querySelector(".mobile-nav-block");
+
+for (var i = 0; i <= mobileMenuButtons.length; i++) {
+    let mobileMenuButton = mobileMenuButtons[i];
+    mobileMenuButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        mobileNav.classList.toggle("open-mobile-nav");
+    });
+}
 
 /**
  * Next, we will create a fresh React component instance and attach it to
--- resources/views/layouts/_partials/_navbar.blade.php
+++ resources/views/layouts/_partials/_navbar.blade.php
@@ -1,26 +1,28 @@
 <!-- main navbar -->
 <nav class="navbar main-navbar navbar-expand-sm">
-    <a href="#" class="navbar-brand">{{ config("app.name") }}</a>
-    <!--
-    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent">
-        <i data-feather="menu"></i>
-    </button>
-    -->
-    <div class="collapse navbar-collapse" id="navbarContent">
+    <a href="/" class="navbar-brand">{{ config("app.name") }}</a>
+    <div class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="/"><i data-feather="home"></i>Accueil</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="#"><i data-feather="globe"></i>Découvrir</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i data-feather="bell"></i>Notifications</a>
             </li>
         </ul>
         <ul class="navbar-nav">
+            <form class="form-inline">
+                <input class="form-control" type="search" placeholder="Rechercher sur {{ config("app.name") }}">
+                <button class="btn btn-search"><i class="search-icon" data-feather="search"></i></button>
+            </form>
             <li class="nav-item">
-                <a class="nav-link" href="#">Home 2</a>
+                <a class="nav-link" href="{{ url("login") }}">Login</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Home 2</a>
+                <a class="nav-link" href="{{ url("register") }}">Register</a>
             </li>
         </ul>
     </div>
@@ -28,27 +30,42 @@
 <!-- end main navbar -->
 
 <!-- mobile navbar -->
-<div class="container">
-    <div class="mobile-navbar">
-        <ul class="nav-block-left">
-            <li class="link-btn">
-                <a href="#">Login</a>
-            </li>
-            <li class="link-btn">
-                <a href="#">Register</a>
-            </li>
-        </ul>
-        <a class="btn-nav-toogle" href="#"><i data-feather="menu"></i> Menu</a>
-        <div class="mobile-nav-block"><!-- open-mobile-nav -->
-            <h4>Navigation</h4>
-            <a href="#" class="btn-nav-toogle">
-                <span class="barsclose"></span> Close
-            </a>
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="https://kaem.io">Frontpage</a>
-                </li>
-            </ul>
+<div class="main-navbar-mobile">
+    <div class="container">
+        <div class="mobile-navbar">
+            <div class="vertical-align-nav">
+                <ul class="nav-block-left">
+                    <li class="link-btn">
+                        <a href="/">{{ config("app.name") }}</a>
+                    </li>
+                </ul>
+                <a class="btn-nav-toogle" href="#"><i data-feather="menu"></i></a>
+            </div>
+            <div class="mobile-nav-block"><!-- open-mobile-nav -->
+                <h4>Navigation</h4>
+                <a href="#" class="btn-nav-toogle">
+                    <i data-feather="x-circle"></i>
+                </a>
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#"><i class="icon" data-feather="home"></i>Accueil</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="icon" data-feather="globe"></i>Découvrir</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="icon" data-feather="bell"></i>Notifications</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#" class="log-link"><i class="icon" data-feather="log-in"></i>Login</a>
+                    </li>
+                    <li>
+                        <a href="#" class="log-link"><i class="icon" data-feather="user-plus"></i>Register</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
