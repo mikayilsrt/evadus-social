5e2ad95edb0e7f1ec994ccb2f728a94a8c8ad995
Mikayil S <sert.mikayil@gmail.com>
2018-10-23 23:47:30 +0200

    Adding the home feed.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,13 +5,124 @@
     <div class="container" id="page-container">
         <div class="row">
             <div class="col-12 col-md-8" id="timeline">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorum ea ex impedit ipsa molestiae necessitatibus nostrum perspiciatis reiciendis, reprehenderit sed ullam, voluptatem. Delectus esse fugiat ipsa possimus recusandae vitae.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorum ea ex impedit ipsa molestiae necessitatibus nostrum perspiciatis reiciendis, reprehenderit sed ullam, voluptatem. Delectus esse fugiat ipsa possimus recusandae vitae.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorum ea ex impedit ipsa molestiae necessitatibus nostrum perspiciatis reiciendis, reprehenderit sed ullam, voluptatem. Delectus esse fugiat ipsa possimus recusandae vitae.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorum ea ex impedit ipsa molestiae necessitatibus nostrum perspiciatis reiciendis, reprehenderit sed ullam, voluptatem. Delectus esse fugiat ipsa possimus recusandae vitae.</p>
+                <div class="content-timeline">
+                    <div class="item-header">
+                        <a href="">
+                            <div class="user-profile-img">
+                                <img src="https://kaem.io/assets/img/default-profile-img.png" alt="">
+                            </div>
+                        </a>
+                        <ul class="">
+                            <li class="fullGroupName"><a href="#">Username</a></li>
+                            <li class="username"><a href="#">@username</a></li>
+                            <li class="item-date">13 sept.</li>
+                        </ul>
+                    </div>
+                    <div class="item-text-content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan justo sem, vitae sollicitudin ex pulvinar vel. Praesent eget lacinia purus, id condimentum orci. In vulputate nibh quis sapien ultrices vestibulum. Mauris ut velit at leo posuere.
+                    </div>
+                    <div class="item-media-content">
+                        <img src="https://images.unsplash.com/photo-1540281481658-a6ebea61c280?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a46acf5c0244921f4eba50da0dfbf401&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" alt="">
+                    </div>
+                    <div class="item-footer">
+                        <div class="item-action">
+                            <ul>
+                                <li><button><i data-feather="heart" class="icon"></i> Likes</button></li>
+                                <li><button><i data-feather="link" class="icon"></i> Share</button></li>
+                                <li><button><i data-feather="chevron-right" class="icon"></i> Status</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-timeline">
+                    <div class="item-header">
+                        <a href="">
+                            <div class="user-profile-img">
+                                <img src="https://kaem.io/assets/img/default-profile-img.png" alt="">
+                            </div>
+                        </a>
+                        <ul class="">
+                            <li class="fullGroupName"><a href="#">Username</a></li>
+                            <li class="username"><a href="#">@username</a></li>
+                            <li class="item-date">13 sept.</li>
+                        </ul>
+                    </div>
+                    <div class="item-text-content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan justo sem, vitae sollicitudin ex pulvinar vel. Praesent eget lacinia purus, id condimentum orci. In vulputate nibh quis sapien ultrices vestibulum. Mauris ut velit at leo posuere.
+                    </div>
+                    <div class="item-media-content">
+                        <img src="https://images.unsplash.com/photo-1540281481658-a6ebea61c280?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a46acf5c0244921f4eba50da0dfbf401&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" alt="">
+                    </div>
+                    <div class="item-footer">
+                        <div class="item-action">
+                            <ul>
+                                <li><button><i data-feather="heart" class="icon"></i> Likes</button></li>
+                                <li><button><i data-feather="link" class="icon"></i> Share</button></li>
+                                <li><button><i data-feather="chevron-right" class="icon"></i> Status</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-timeline">
+                    <div class="item-header">
+                        <a href="">
+                            <div class="user-profile-img">
+                                <img src="https://kaem.io/assets/img/default-profile-img.png" alt="">
+                            </div>
+                        </a>
+                        <ul class="">
+                            <li class="fullGroupName"><a href="#">Username</a></li>
+                            <li class="username"><a href="#">@username</a></li>
+                            <li class="item-date">13 sept.</li>
+                        </ul>
+                    </div>
+                    <div class="item-text-content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan justo sem, vitae sollicitudin ex pulvinar vel. Praesent eget lacinia purus, id condimentum orci. In vulputate nibh quis sapien ultrices vestibulum. Mauris ut velit at leo posuere.
+                    </div>
+                    <div class="item-media-content">
+                        <img src="https://images.unsplash.com/photo-1540281481658-a6ebea61c280?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a46acf5c0244921f4eba50da0dfbf401&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" alt="">
+                    </div>
+                    <div class="item-footer">
+                        <div class="item-action">
+                            <ul>
+                                <li><button><i data-feather="heart" class="icon"></i> Likes</button></li>
+                                <li><button><i data-feather="link" class="icon"></i> Share</button></li>
+                                <li><button><i data-feather="chevron-right" class="icon"></i> Status</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-timeline">
+                    <div class="item-header">
+                        <a href="">
+                            <div class="user-profile-img">
+                                <img src="https://kaem.io/assets/img/default-profile-img.png" alt="">
+                            </div>
+                        </a>
+                        <ul class="">
+                            <li class="fullGroupName"><a href="#">Username</a></li>
+                            <li class="username"><a href="#">@username</a></li>
+                            <li class="item-date">13 sept.</li>
+                        </ul>
+                    </div>
+                    <div class="item-text-content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur accumsan justo sem, vitae sollicitudin ex pulvinar vel. Praesent eget lacinia purus, id condimentum orci. In vulputate nibh quis sapien ultrices vestibulum. Mauris ut velit at leo posuere.
+                    </div>
+                    <div class="item-media-content">
+                        <img src="https://images.unsplash.com/photo-1540281481658-a6ebea61c280?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a46acf5c0244921f4eba50da0dfbf401&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" alt="">
+                    </div>
+                    <div class="item-footer">
+                        <div class="item-action">
+                            <ul>
+                                <li><button><i data-feather="heart" class="icon"></i> Likes</button></li>
+                                <li><button><i data-feather="link" class="icon"></i> Share</button></li>
+                                <li><button><i data-feather="chevron-right" class="icon"></i> Status</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
             @include("layouts._partials._block-section")
         </div>
-        <h1>Home page, user id logged.</h1>
     </div>
 @endsection
