c3cc0d63ee27f1876395c9bb64323ef0a2a14d17
Mikayil S <sert.mikayil@gmail.com>
2018-10-25 00:11:43 +0200

    Create collection block and style.

--- resources/views/layouts/_partials/_block-section.blade.php
+++ resources/views/layouts/_partials/_block-section.blade.php
@@ -1,6 +1,41 @@
-<div class="block-section col-12 col-md-4">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis cum dignissimos doloremque eligendi fugit magni nobis nostrum odio officia qui quidem rem, rerum sequi velit voluptatem voluptates. Adipisci, molestias?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis cum dignissimos doloremque eligendi fugit magni nobis nostrum odio officia qui quidem rem, rerum sequi velit voluptatem voluptates. Adipisci, molestias?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis cum dignissimos doloremque eligendi fugit magni nobis nostrum odio officia qui quidem rem, rerum sequi velit voluptatem voluptates. Adipisci, molestias?</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda corporis cum dignissimos doloremque eligendi fugit magni nobis nostrum odio officia qui quidem rem, rerum sequi velit voluptatem voluptates. Adipisci, molestias?</p>
+<div class="block-section col-12 col-md-5">
+    <div class="block-title">
+        <h3>Collections</h3>
+    </div>
+    <div class="row">
+        <div class="collection-item col-md-6">
+            <a href="">
+                <div class="content-item" style="background: url('https://images.unsplash.com/photo-1514866747592-c2d279258a78?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=35bb216e4f81c3bde24544b5630f5bd6&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb')">
+                    <div class="header-collection-item">
+                        <div class="user-profile-img">
+                            <img src="https://kaem.io/assets/img/default-profile-img.png" alt="">
+                        </div>
+                    </div>
+                    <div class="footer-collection-item">
+                        <div class="footer-content-item">
+                            <h4>Fashion Talk</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare posuere neque a vestibulum.</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="collection-item col-md-6">
+            <a href="">
+                <div class="content-item" style="background: url('https://images.unsplash.com/photo-1503301019213-64c01f106990?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=78a2a1f996ac2b0dd282eef9ba7c6a82&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb')">
+                    <div class="header-collection-item">
+                        <div class="user-profile-img">
+                            <img src="https://kaem.io/assets/img/default-profile-img.png" alt="">
+                        </div>
+                    </div>
+                    <div class="footer-collection-item">
+                        <div class="footer-content-item">
+                            <h4>Smoke Show</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ornare posuere neque a vestibulum.</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
 </div>
