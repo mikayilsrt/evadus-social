692a828034cf72ef947560701666177eae2dcea4
Mikayil S <sert.mikayil@gmail.com>
2018-11-11 21:12:05 +0100

    Adding the auth form in login and register page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,31 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container">
-        <h1>Login page</h1>
+    <div id="content-auth">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-8 col-md-6 left-content">section 1</div>
+                <div class="col-12 col-lg-4 col-md-6 right-content">
+                    <h1>Join {{ config("app.name") }} today</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad autem.</p>
+                    <div class="form-content">
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="email@example.com">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" placeholder="password">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-auth">Log in</button>
+                            </div>
+                            <div class="form-group">
+                                <p>New on {{ config("app.name") }} ? <a href="{{ url("register") }}"> Register now</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,37 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container">
-        <h1>Register page</h1>
+    <div id="content-auth">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-8 col-md-6 left-content">section 1</div>
+                <div class="col-12 col-lg-4 col-md-6 right-content">
+                    <h1>Join {{ config("app.name") }} today</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad autem.</p>
+                    <div class="form-content">
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="username">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="email@example.com">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" placeholder="password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" placeholder="Confirm your password">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-auth">Sing up</button>
+                            </div>
+                            <div class="form-group">
+                                <p>En cliquant sur "S'inscrire" ci-dessous, vous souscrivez aux <a href="">règles de l’instance</a> et à nos <a href="">conditions d’utilisation</a>.</p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
