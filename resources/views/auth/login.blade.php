5055caad4482bfa8708e6a691a6b07391bfbb142
Mikayil S <sert.mikayil@gmail.com>
2018-11-21 22:01:42 +0100

    Adding the login controller in form action.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,12 +9,13 @@
                     <h1>Join {{ config("app.name") }} today</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad autem.</p>
                     <div class="form-content">
-                        <form method="POST" action="">
+                        <form method="POST" action="{{ url("login") }}">
+                            @csrf
                             <div class="form-group">
-                                <input type="email" class="form-control" placeholder="email@example.com">
+                                <input type="email" class="form-control" name="email" placeholder="email@example.com">
                             </div>
                             <div class="form-group">
-                                <input type="password" class="form-control" placeholder="password">
+                                <input type="password" class="form-control" name="password" placeholder="password">
                             </div>
                             <div class="form-group">
                                 <button type="submit" class="btn btn-auth">Log in</button>
