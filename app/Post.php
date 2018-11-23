583727a0cf03946ce44ce8902991d85d575f6e0e
Mikayil S <sert.mikayil@gmail.com>
2018-11-24 00:49:15 +0100

    Add a relationship on post model.

---
+++ app/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        "content", "media"
+    ];
+
+    /**
+     * Get the user that owns the post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user () {
+        return $this->belongsTo(Post::class);
+    }
+}
