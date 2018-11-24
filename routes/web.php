8e6ba39715ad8aed1b134d5982373e61b01e6a5a
Mikayil S <sert.mikayil@gmail.com>
2018-11-24 20:24:06 +0100

    Add home controller and remove home constructor.

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/', function () {
-        return view('home.index');
-    });
+    Route::get('/', "HomeController@index");
     
     Route::get('/discover', function () {
         return view('discover.index');
