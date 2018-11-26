71dc989277b9cc1da02a6b6c4be733ea9d6eae5c
Mikayil S <sert.mikayil@gmail.com>
2018-11-26 20:08:26 +0100

    Adding the like controller, model & migration

---
+++ database/migrations/2018_11_26_183354_create_likes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments("id");
+            $table->integer("user_id")->nullable(false);
+            $table->boolean("like")->nullable(false)->default(true);
+            $table->timestamps();
+            $table->foreign("user_id")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
