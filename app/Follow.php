cf1374582ea2600af6b4363ef23541cd33c8610f
Mikayil S <sert.mikayil@gmail.com>
2018-12-12 20:58:12 +0100

    Add comments on Follow model.

--- app/Follow.php
+++ app/Follow.php
@@ -6,10 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Follow extends Model
 {
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
     protected $fillable = [
         "follower_id", "following_id"
     ];
 
+    /**
+     * Get the user that owns Follow.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function user () {
         return $this->hasOne(User::class);
     }
